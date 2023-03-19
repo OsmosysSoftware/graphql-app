2010441e4855e91da11bf593402c724b3e53e374
ayush-osmosys <ayush@osmosys.co>
2023-03-19 20:13:27 +0530

    modify console statement when server start

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import schema from "./graphql/index.js";
 
+const port = 4000;
+
 const server = new ApolloServer({
   schema,
 });
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port },
 });
-console.log("server started at port " + 4000);
+console.log(`aserver started at ${url}:${port}`);
