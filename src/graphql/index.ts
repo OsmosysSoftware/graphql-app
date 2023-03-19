e13c982048bc82a8f60f3cb147e6647c030499fb
ayush-osmosys <ayush@osmosys.co>
2023-03-20 01:45:46 +0530

    add junction table bookandgenre type

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,9 @@
 import "./types/Query.js";
 import "./types/Book.js";
 import "./types/Author.js";
+import "./types/Genre.js";
 import "./types/BooksAndAuthors.js";
+import "./types/BooksAndGenres.js";
 
 import builder from "../utils/schemaBuilder.js";
 const schema = builder.toSchema();
