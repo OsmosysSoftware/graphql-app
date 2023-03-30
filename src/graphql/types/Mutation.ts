f45a899ea10d8465339b2ad44ed10f216c0ce8cb
ayush <ayush@osmosys.co>
2023-03-30 20:03:45 +0530

    add provision to add genre in book

--- src/graphql/types/Mutation.ts
+++ src/graphql/types/Mutation.ts
@@ -1,38 +1,60 @@
 import prisma  from "../../utils/PrismaClient.js";
 import builder from "../../utils/schemaBuilder.js";
+import { AuthorInput, BookInput, genreInput } from "./input.js";
 
 const mutation=builder.mutationType({
     fields:t=>({
         addBook:t.prismaField({
             type:"Book",
             args:{
-                name:t.arg.string({required:true}),
-                authors:t.arg.stringList({required:true}),
-                description:t.arg.string()
+                book:t.arg({type:BookInput,required:true}),
+                authors:t.arg({type:[AuthorInput],required:true}),
+                genres:t.arg({type:[genreInput]}),
             },
             
             
             resolve:async (_,__,args)=>{
+                const {name:bookName,description:bookDescription}=args.book;
+                const {genres,authors}=args;
+                
                 return await prisma.book.create({
                    data:{
-                    name:args.name,
-                    description:args.description,
+                    name:bookName,
+                    description:bookDescription,
                     authors:{
-                       create:args.authors.map(author=>{
+                       create:authors.map(_author=>{
                         return {
                             author:{
                                 connectOrCreate:{
                                     where:{
-                                        name:author
+                                        name:_author.name
                                     },
                                     create:{
-                                        name:author
+                                        name:_author.name,
+                                        email:_author.email
                                     }
                                 }
                             }
                         }
 
                        } )
+                    },
+                    genres:{
+                        create:genres?.map(_genre=>{
+                           return {
+                            genre:{
+                                connectOrCreate:{
+                                    where:{
+                                        name:_genre.name
+                                    },
+                                    create:{
+                                        name:_genre.name
+                                    }
+                                }
+                            }
+                           }
+                        })
+
                     }
                    }
 
---
+++ src/graphql/types/input.ts
@@ -0,0 +1,23 @@
+import builder from "../../utils/schemaBuilder.js";
+
+export const AuthorInput=builder.inputType("authorInput",{
+    fields:t=>({
+        name:t.string({required:true}),
+        email:t.string(),
+
+    })
+    
+})
+export const BookInput=builder.inputType("bookInput",{
+    fields:t=>({
+        name:t.string({required:true}),
+        description:t.string(),
+
+    })
+})
+export const genreInput=builder.inputType("genreType",{
+    fields:t=>({
+        name:t.string({required:true}),
+    })
+})
+
